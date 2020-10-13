81e14c1998dfadfadab2f9645dec9d7687df9872
Rajit Deb <rajitdeb456@gmail.com>
2020-10-13 18:39:13 +0530

    Added material design components and also handled validation

--- app/src/main/java/com/abhishek/androidloginui/MainActivity.kt
+++ app/src/main/java/com/abhishek/androidloginui/MainActivity.kt
@@ -2,17 +2,24 @@ package com.abhishek.androidloginui
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var usernameEdt: EditText
-    private lateinit var passwordEdt: EditText
+    private lateinit var usernameEdt: TextInputEditText
+
+    private lateinit var passwordEdtLayout: TextInputLayout
+    private lateinit var passwordEdt: TextInputEditText
+
     private lateinit var loginBtn: Button
     private lateinit var signUpBtn: TextView
 
@@ -21,18 +28,31 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         usernameEdt = findViewById(R.id.username_edt)
+
         passwordEdt = findViewById(R.id.password_edt)
+        passwordEdtLayout = findViewById(R.id.password_edt_layout)
+
+        passwordEdt.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                passwordEdtLayout.error = null
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                passwordEdt.error = null
+            }
+
+
+        })
 
         loginBtn = findViewById(R.id.login_btn)
         signUpBtn = findViewById(R.id.signUp_btn)
 
-        loginBtn.setOnClickListener{
-            login()
-        }
+        loginBtn.setOnClickListener { login() }
 
-        signUpBtn.setOnClickListener{
-            signUp()
-        }
+        signUpBtn.setOnClickListener { signUp() }
 
     }
 
@@ -44,17 +64,25 @@ class MainActivity : AppCompatActivity() {
         val username: String = "raju@babubhaiyya.com"
         val password: String = "ye_raju_kaStyle_Hai@39"
 
-        if(getUsernameFromEdt() == username && getPasswordFromEdt() == password){
+        if (getUsernameFromEdt() == username && getPasswordFromEdt() == password) {
             Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show()
+        } else if(getUsernameFromEdt() == username && getPasswordFromEdt() != password){
+            passwordEdtLayout.error = "Invalid password!"
         }else{
-            Toast.makeText(this, "Login Unsuccessful", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this,
+                "Login Unsuccessful. Invalid username!",
+                Toast.LENGTH_SHORT)
+                .show()
         }
     }
 
-    private fun getUsernameFromEdt(): String{ return usernameEdt.text.trim().toString() }
+    private fun getUsernameFromEdt(): String {
+        return usernameEdt.text!!.trim().toString()
+    }
 
-    private fun getPasswordFromEdt(): String{ return passwordEdt.text.toString() }
-
+    private fun getPasswordFromEdt(): String {
+        return passwordEdt.text.toString()
+    }
 
 
 }
